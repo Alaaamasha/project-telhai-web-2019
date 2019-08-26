9ab8c67be90c5e906d530957f7f1d21f90a6f5eb
AlaaAmasha <alaa.amasha.3@gmail.com>
2019-08-26 16:38:12 +0300

    .....

--- web-project/src/app/app.component.ts
+++ web-project/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'social-web-app';
+  title = 'angular-material-login-template';
 }
